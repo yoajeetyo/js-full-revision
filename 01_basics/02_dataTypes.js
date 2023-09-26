e10463a642be45678620b635f4e4d9d1c44821be
yoajeetyo <sajeetkumar40@gmail.com>
2023-09-26 22:33:50 +0530

    java script data-types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,38 @@
+"use strict"; //treat all js code as newer version (this is not required in current version, just for understanding)
+
+// alert("Hello")
+// we are using nodejs, not browser so it will give error
+
+console.log(  
+    3
+    +
+    3)          // this code will work but not preferred because it will decrease readability
+
+console.log("Ajeet");
+
+
+let name ="Ajeet"
+let age = 18
+let isLoggedIn = true
+
+
+//Premitive data-types
+
+// number => 2^53
+// bigint => for very big numbers
+// String =>"" or ''
+// boolean => true/false
+// null => standalone value(khali hai)
+// undefined => still not defined data-type
+// symbol => unique (used to define unique components in ReactJs)
+
+// Object
+
+console.log(typeof "Ajeet");    // string
+console.log(typeof 2);          //number
+console.log(typeof 3.14);       // number
+console.log(typeof null);       // object
+console.log(typeof undefined);  // undefined
+
+// null is an object
+// undefined is a type of undefined
