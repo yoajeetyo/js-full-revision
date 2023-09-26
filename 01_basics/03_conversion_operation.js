231f63c5cb93edbf4246747672e063335b018712
yoajeetyo <sajeetkumar40@gmail.com>
2023-09-27 00:15:41 +0530

    conversion of data-type

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,61 @@
+let score1 = "33"
+
+console.log(typeof score1);
+
+// ---------or----------
+
+console.log(typeof(score1));
+
+let valueInNumber1 = Number(score1)
+console.log(valueInNumber1);         // 33 
+console.log(typeof valueInNumber1);  // number
+
+
+let score2 = "33abcd"
+let valueInNumber2 = Number(score2)
+console.log(valueInNumber2);         // NaN (not a number) (because in score2 string there is not only number so it will convert but return NaN)
+console.log(typeof valueInNumber2);  // number
+
+
+let score3 = null
+let valueInNumber3 = Number(score3)
+console.log(valueInNumber3);         // 0 (conversion of null to number will return 0)
+console.log(typeof valueInNumber3);  // number
+
+
+let score4 = undefined
+let valueInNumber4 = Number(score4)
+console.log(valueInNumber4);         // NaN (conversion of undefined to number will return NaN)
+console.log(typeof valueInNumber4);  // number
+
+
+let score5 = "Ajeet"
+let valueInNumber5 = Number(score5)
+console.log(valueInNumber5);         // NaN (conversion of string to number will return NaN)
+console.log(typeof valueInNumber5);  // number
+
+
+let score6 = true
+let valueInNumber6 = Number(score6)
+console.log(valueInNumber6);         // 1 (conversion of boolean(true/false) to number will return (1/0))
+console.log(typeof valueInNumber6);  // number
+
+
+// conversion to boolean
+let isLoggedIn1 = -1
+let booleanIsLoggedIn1 = Boolean(isLoggedIn1)
+console.log(booleanIsLoggedIn1);         // true (conversion of number to boolean will return (true for non-zero/false for 0) )
+console.log(typeof booleanIsLoggedIn1);  // boolean
+
+
+let isLoggedIn2 = " "
+let booleanIsLoggedIn2 = Boolean(isLoggedIn2)
+console.log(booleanIsLoggedIn2);         // true (conversion of string to boolean will return (true for non-empty string/false for empty string) )
+console.log(typeof booleanIsLoggedIn2);  // boolean
+
+
+// conversion to string
+let someNumber1 = 44
+let stringSomeNumber1 = String(someNumber1)
+console.log(stringSomeNumber1);         // "44"
+console.log(typeof stringSomeNumber1);  // string
