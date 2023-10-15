5631ebe42143ea28dfdd14c16f299ca6a466bc27
yoajeetyo <sajeetkumar40@gmail.com>
2023-10-15 19:50:46 +0530

    04 objects more methods and concepts on object

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,108 @@
+// singleton object or object using constructor
+
+// const myObjectAsLiterals = {} // non-singleton object
+// const myObjectAsConstructor = new Object() // singleton object
+
+// // there is no difference between both the object only difference is one is singleton and another one is non-singleton
+// // when we print both object we will get same result
+
+// console.log(myObjectAsLiterals); // {}
+// console.log(myObjectAsConstructor); // {}
+
+
+// object inside an object
+// const regularUser = {
+//     email : "abc@gmail.com",
+//     fullname : {
+//         userfullname : {
+//             firstname : "Ajeet",
+//             lastname : "Singh"
+//         }
+//     }
+// }
+
+// //accessing firstname
+// console.log(regularUser.fullname.userfullname.firstname); // Ajeet
+
+
+
+// merging multiple objects as single object
+// const obj1 = {1 : "A", 2 : "B"}
+// const obj2 = {3 : "C", 4 : "D"}
+
+// this way is not recommended
+// const obj3 = {obj1, obj2} // worst way (it will put obj1 and obj2 inside obj3)
+// console.log(obj3); // { obj1: { '1': 'A', '2': 'B' }, obj2: { '3': 'C', '4': 'D' } }
+
+// this way is also not recommended but sometimes it is used
+// here first argument is target and other all are source means all the objects are going in first object only
+// const obj3 = Object.assign( obj1, obj2 )
+// console.log(obj3); // { '1': 'A', '2': 'B', '3': 'C', '4': 'D' }
+
+// this way is better way and recommended but not much in use, btw {} is optional parameter but better to give
+// here first argument is target and other all are source means all the objects are stored in first object which is {}
+// const obj3 = Object.assign( {}, obj1, obj2 )
+// console.log(obj3); // { '1': 'A', '2': 'B', '3': 'C', '4': 'D' }
+
+// best way is spread operator
+// const obj3 = {...obj1, ...obj2}
+// console.log(obj3); // { '1': 'A', '2': 'B', '3': 'C', '4': 'D' }
+
+
+// when objects are coming from database (from database values always comes as an array)
+// const users = [
+//     {
+//         id : 1,
+//         email : "abc@gmail.com"
+//     },
+//     {
+//         id : 2,
+//         email : "def@gmail.com"
+//     },
+//     {
+//         id : 3,
+//         email : "ghi@gmail.com"
+//     },
+//     {
+//         id : 4,
+//         email : "jkl@gmail.com"
+//     },
+// ]
+
+// inside array there are so many objects data and we want to loop through or we want to print values
+//console.log(users[1].email) // def@gmail.com (it will return 1 index email in array)
+
+
+
+
+
+
+// suppose we want all the keys or values of object as an array
+// const regularUser = {
+//     id : 1,
+//     name : "Ajeet",
+//     isLoggedIn : false
+// }
+
+// // to get all keys
+// console.log(Object.keys(regularUser)); // [ 'id', 'name', 'isLoggedIn' ]
+
+// // to get all the values
+// console.log(Object.values(regularUser)); // [ 1, 'Ajeet', false ]
+
+// // to get all entries (keys and values)
+// console.log(Object.entries(regularUser)); // [ [ 'id', 1 ], [ 'name', 'Ajeet' ], [ 'isLoggedIn', false ] ]  (return arrays of array of key and values)
+
+
+// // to check whether a given property is present in the object or not
+// console.log(regularUser.hasOwnProperty('isLoggedIn')); // true
+// console.log(regularUser.hasOwnProperty('isLoggedOut')); // false
+
+
+
+
+
+
+
+
+
