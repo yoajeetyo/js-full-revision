e9ec858acca8135a1e722f535928d003c79ad6ae
yoajeetyo <sajeetkumar40@gmail.com>
2023-10-24 00:37:44 +0530

    for in loop

---
+++ 05_loopsOrIterations/four.js
@@ -0,0 +1,53 @@
+// object iteration
+// for of loop not work in object iteration
+// we will use for in loop for the iteration of object
+
+// const myObj = {
+//     js : "javascript",
+//     cpp : "C++",
+//     java : "java",
+//     py : "python"
+// }
+
+// for (const key in myObj) {
+//     console.log(key, ":-", myObj[key]);
+// }
+
+
+// Output:- js :- javascript
+        // cpp :- C++
+        // java :- java
+        // py :- python
+
+
+
+
+
+// iterating array using for in loop
+// const names = ["Ajeet", "Shyam"]
+// for (const key in names) {
+//     console.log(names[key]);
+// }
+
+// Output:- Ajeet
+        // Shyam
+
+
+
+
+// for in loop always return the key of object and index of array
+
+
+
+
+
+// we can not iterate map using for in loop
+
+
+
+
+
+
+
+
+
