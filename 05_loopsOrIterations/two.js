8d42f04457d2c641c39b149b37b1ff41c731e84d
yoajeetyo <sajeetkumar40@gmail.com>
2023-10-23 18:41:00 +0530

    while loop

---
+++ 05_loopsOrIterations/two.js
@@ -0,0 +1,25 @@
+// while loop
+
+
+// let num = 0
+// while (num <= 10) {
+//     console.log(`value of num is ${num}`);
+//     num = num + 2
+// }
+
+
+
+
+
+// printing array element using while loop
+// const myHero = ["Batman", "Superman", "Flash", "Nagraj"]
+// let index = 0
+// while (index < myHero.length) {
+//     console.log(`Heros are ${myHero[index]}`);
+//     index++
+// }
+
+
+
+
+
