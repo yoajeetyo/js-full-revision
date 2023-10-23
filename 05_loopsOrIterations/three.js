6d6257ed51f384ad3572ba55a092a26c58b64180
yoajeetyo <sajeetkumar40@gmail.com>
2023-10-23 23:54:17 +0530

    for of loop

---
+++ 05_loopsOrIterations/three.js
@@ -0,0 +1,44 @@
+// Array specific loops
+// mostly from API's and Database we will get Arrays or Array of Objects so in that situation we will use these loops
+
+// ["", "", "", ""]
+
+// [{}, {}, {}, {}]
+
+
+// for of loop   (Array, object, String)
+
+// syntax :-
+
+// for (const iterator of object) {    
+// }
+
+// iterator will iterate the object, string or array
+// in place of object we can write string, object or array
+
+
+
+// const arr =[1, 2, 3, 4, 5, 6, 7, 8]
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+
+
+// for of loop on String
+// const greeting = "Hello World!"
+// for (const greet of greeting) {
+//     console.log(`Each character in greeting string is: ${greet}`);
+// }
+
+
+
+
+
+
+
+
+
+
+
