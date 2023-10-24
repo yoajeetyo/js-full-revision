3bf55e827505dd43d842fd360ebdfd982f4624b1
yoajeetyo <sajeetkumar40@gmail.com>
2023-10-24 13:44:22 +0530

    map() method

---
+++ 05_loopsOrIterations/seven.js
@@ -0,0 +1,13 @@
+// map() function
+
+// suppose we want to add 10 in all the elements in an number array
+
+// const arr = [1,2,3,4,5,6,7,8,9,10]
+// let newArr = arr.map((val) => {return val + 10})
+// console.log(newArr);
+
+
+// output:- [
+        // 11, 12, 13, 14, 15,
+        // 16, 17, 18, 19, 20
+        // ]
