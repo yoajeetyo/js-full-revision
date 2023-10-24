761ae61d1575ad549bfcbe647bf8da4b5f167472
yoajeetyo <sajeetkumar40@gmail.com>
2023-10-24 13:56:39 +0530

    method chaining

--- 05_loopsOrIterations/seven.js
+++ 05_loopsOrIterations/seven.js
@@ -11,3 +11,35 @@
         // 11, 12, 13, 14, 15,
         // 16, 17, 18, 19, 20
         // ]
+
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++ method chaining +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// if we wan to use multiple method on same object, then we can use method chaining
+
+
+// first multiply all element with 10(map) then add 1(map) in each element, then return all the element which is >= 40 (filter)
+// const arr = [1,2,3,4,5,6,7,8,9,10]
+// let newArr = arr
+//                 .map((val) => {return val * 10})
+//                 .map((val) => {return val + 1} )    // next chained method always performed operation previuos returned value
+//                 .filter((val) => {return val >= 40}) // next chained method always performed operation previuos returned value
+
+// console.log(newArr);
+
+
+// output:- [
+        // 41, 51,  61, 71,
+        // 81, 91, 101
+        // ]
+
+
+
+
+
+
+
+
