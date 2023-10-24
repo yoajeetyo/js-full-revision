7de1cb1fdd8954fe85ab097301d4920c41c5e911
yoajeetyo <sajeetkumar40@gmail.com>
2023-10-24 16:00:50 +0530

    reduce() method

---
+++ 05_loopsOrIterations/eight.js
@@ -0,0 +1,58 @@
+// reduce() method
+
+
+
+// to get the sum of all elements in array
+// const nums = [1, 2, 3, 4, 5]
+
+// const initial = 0
+// let sumElement = nums.reduce((acc, currVal) => {return acc + currVal}, initial)
+// console.log(sumElement); // 15
+
+// first time acc will take initial value which is 0, and currVal will take 1, and return 0+1 which is 1
+// second time acc will take returned value 1 value, and currVal will take 2, and return 1+2 which is 3
+// and so on, at last it will return sum of all elements
+
+
+
+
+// Returning total price of shopping from shopping cart
+
+// const myCart = [
+//     {
+//         item : "Banana",
+//         price : 50
+//     },
+//     {
+//         item : "Mango",
+//         price : 80
+//     },
+//     {
+//         item : "Cherry",
+//         price : 100
+//     },
+//     {
+//         item : "Guava",
+//         price : 40
+//     },
+//     {
+//         item : "Grapes",
+//         price : 60
+//     }
+// ]
+
+// const initialPrice = 0
+// const totalPrice = myCart.reduce((acc, item) => {return acc + item.price}, initialPrice)
+// console.log(totalPrice);
+
+
+
+// output:- 330
+
+
+
+
+
+
+
+
