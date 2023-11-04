c74802adce877a4be8aa4c6bccbec81354ab7ad3
yoajeetyo <sajeetkumar40@gmail.com>
2023-11-04 19:30:38 +0530

    async theory

---
+++ 12_Async_code/async_code_theory.js
@@ -0,0 +1,60 @@
+// JS is synchronous code by default
+// single threaded language by default
+// compare to some(multi threaded) language it is slow language
+
+
+
+
+// execution context
+
+// executes one line of code at a time
+// each operation waits for the last one to complete before executing
+
+// console.log("one");
+// console.log("two");
+
+// log-one will execute first then log-two will execute (log-two will wait for log-one to get executed) 
+
+// call stack and memory heap also getting created
+
+
+
+
+//// Blocking code vs Non Blocking code
+
+// Blocking code will block the flow of program (executes file Synchronousley)
+
+// Non Blocking code does not block the flow of program (executes file Asynchronousley)
+
+// Browsers does not have file reading functionality, NodeJs have this functionality
+
+
+// based on use case we need to write blocking or non-blocking code
+
+
+
+
+
+
+
+
+// for JS engine and Web API diagram follow the video 4 hr 9 minutes (js-2)
+// In JS engine only memory heap and call stack is there
+// If we want we can take web API or Node Js environment saperately
+// if we will take web API then we will get DOM API
+// if we will take Node js environment then DOM API is not present
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
